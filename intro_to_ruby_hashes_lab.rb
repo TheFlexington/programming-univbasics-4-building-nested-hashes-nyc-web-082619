3075ee8be120cde78a41f53053ac115f46df19a7
Felix Martinez <felixaviermartinez@gmail.com>
2019-08-08 19:17:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,9 +32,9 @@ def monopoly_with_third_tier
       }, 
     :names => {
       :reading_railroad => {}, 
-      :two => '', 
-      :three => '', 
-      :four => ''
+      :pennsylvania_railroad => {}, 
+      :three => {}, 
+      :four => {}
       }
     }
   }
